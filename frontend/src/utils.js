f6f7addc876e964b8aff08a665897f3887e54278
Pavel Poltavets <poltavets-pavel@mail.ru>
2019-07-24 14:53:14 +0300

    Phone validation regex (\d{4}) changed to pair of digits: (\d{2})  - the required exchange/subscriber number; [-. ]*  - certain non numeric characters

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
 // http://regexlib.com/Search.aspx?k=phone&c=7&m=-1&ps=20  - more regex for phone numbers
-const phoneRegex = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+const phoneRegex = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{2})[-. ]*(\d{2})(?: *x(\d+))?\s*$/;
 const isPhoneValid = phone => !!phone && phoneRegex.test(phone);
 
 // https://emailregex.com/
@@ -8,6 +8,6 @@ const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+")
 const isEmailValid = email => !!email && emailRegex.test(email);
 
 export default {
-	isPhoneValid: isPhoneValid,
-	isEmailValid: isEmailValid
-}
+	isPhoneValid,
+	isEmailValid
+};
